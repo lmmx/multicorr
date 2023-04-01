689e16be40d943e01ed71c178743f71cf803e651
Louis Maddox <louismmx@gmail.com>
2023-04-01 12:25:37 +0100

    Rename null target to target

--- corr_analysis_multicat_dummy_form.py
+++ corr_analysis_multicat_dummy_form.py
@@ -8,12 +8,12 @@ data = pd.read_csv("simple_dataset.csv")
 
 cat_cols = ["c1", "c2", "c3"]
 target_column = "v"
-null_target = "null_target_column"
+target = "null_target_column"
 
 # Create a binary column to indicate the presence of null values in the target column
 null_indicator = data[target_column].isnull()
-data[null_target] = null_indicator.astype(int)
-original_cols = list(data.columns)
+data[target] = null_indicator.astype(int)
+original_cols = data.columns
 
 
 def name_multicat(s: pd.Series) -> str:
@@ -45,11 +45,10 @@ for index, row in data.iterrows():
 
 # Calculate the correlation between the binary column and the new columns
 dummy_cols = data.columns.drop(original_cols)
-corr_cols = [null_target, *dummy_cols]
-correlation_matrix = data[corr_cols].corr(numeric_only=True)
+correlation_matrix = data[[target, *dummy_cols]].corr(numeric_only=True)
 
 # Display the correlations between the binary column and the new columns
-correlations_with_null_target = correlation_matrix[null_target].loc[dummy_cols]
-print(correlations_with_null_target)
+correlations_with_target = correlation_matrix[target].loc[dummy_cols]
+print(correlations_with_target)
 
-assert correlations_with_null_target.max() == 1.0
+assert correlations_with_target.max() == 1.0
